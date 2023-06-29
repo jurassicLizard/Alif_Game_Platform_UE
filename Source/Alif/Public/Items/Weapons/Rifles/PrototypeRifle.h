4076631d159a6673d726f328f407f9e83e3d414c
Salem Bouraoui <salem.b@hotmail.de>
2023-06-29 18:45:56 +0200

    add new weapon assets

---
+++ Source/Alif/Public/Items/Weapons/Rifles/PrototypeRifle.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/BaseItem.h"
+#include "PrototypeRifle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIF_API APrototypeRifle : public ABaseItem
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
